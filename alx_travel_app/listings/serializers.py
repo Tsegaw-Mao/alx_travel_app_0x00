53af41a930c4502284f944206cfcaf709e4e5a79
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-21 00:22:39 +0300

    serializer

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Listing, Booking
 
+
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = '__all__'
+        read_only_fields = ['listing_id', 'created_at']
 
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+        read_only_fields = ['booking_id', 'created_at']
